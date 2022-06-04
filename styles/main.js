9f235b11dbd9d9a9ca68f2674bc73741ee1d199b
gift56 <efeasiughu@gmail.com>
2022-06-04 16:29:13 +0100

    added circle loop

--- styles/main.js
+++ styles/main.js
@@ -1,3 +1,9 @@
-const loader = document.querySelector('.loader');
-const dots = loader.getAttribute('dots')
-const points = ""
+const circle = document.querySelector('.circle');
+const dots = circle.getAttribute('dots');
+const points = "";
+const rotate = 360 / dots;
+
+for (let i = 0; i < dot; i++) {
+    points += `<div class="point" style="--i: ${i};--rot:${rotate}"></div>`;
+    circle.innerHTML = points
+}
