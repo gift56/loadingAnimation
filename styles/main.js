c92a468c190a7721e81e093634b0169ae131a57f
gift56 <efeasiughu@gmail.com>
2022-06-04 17:07:51 +0100

    for loop is the worst

--- styles/main.js
+++ styles/main.js
@@ -1,9 +1,9 @@
 const circle = document.querySelector('.circle');
 const dots = circle.getAttribute('dots');
-const points = "";
-const rotate = 360 / dots;
+let points = "";
+let rotate = 360 / dots;
 
-for (let i = 0; i < dot; i++) {
-    points += `<div class="point" style="--i: ${i};--rot:${rotate}"></div>`;
-    circle.innerHTML = points
+for (let i = 0; i < dots; i++) {
+    points += `<div class="point" style="--i: ${i}; --rot: ${rotate}"></div>`;
 }
+circle.innerHTML = points;
