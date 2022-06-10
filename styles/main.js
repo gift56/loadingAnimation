7f4056bf94f5301086cff4b66d1aa8d3d608fc93
gift56 <efeasiughu@gmail.com>
2022-06-10 13:41:05 +0100

    removed preloader

--- styles/main.js
+++
@@ -1,9 +0,0 @@
-const circle = document.querySelector('.circle');
-const dots = circle.getAttribute('dots');
-let points = "";
-let rotate = 360 / dots;
-
-for (let i = 0; i < dots; i++) {
-    points += `<div class="point" style="--i: ${i}; --rot: ${rotate}"></div>`;
-}
-circle.innerHTML = points;
